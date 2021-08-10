0b2dfcb438731ff97a86cc739aa3da4073822ef5
Maksim Bazhenov <maksim4k1@gmail.com>
2021-08-10 12:46:11 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import MyInfo from './components/MyInfo';
+import MyName from './components/MyName';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <MyName/>
+            <MyInfo/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/MyName.jsx
@@ -0,0 +1,9 @@
+function MyName(){
+    return(
+        <h1>
+            Максим
+        </h1>
+    );
+}
+
+export default MyName;
